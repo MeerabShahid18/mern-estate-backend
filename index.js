92e80a7bb086e4a241d0e74576600e3772094a39
MeerabShahid18 <meerabshahid866@gmail.com>
2026-03-31 17:06:16 +0500

    recovering app.listen

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const authRouter=require("./routers/auth.router");
 const dotenv=require("dotenv");
 const listingRouter=require('./routers/listing.router');
 const cookieParser = require("cookie-parser");
-// app.use(cors());
+app.use(cors());
 
 dotenv.config();
 
-const cors = require('cors');
-// ✅ CORS CONFIG (FINAL)
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+// const cors = require('cors');
+// // ✅ CORS CONFIG (FINAL)
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
 
-    if (
-      origin.includes("vercel.app") ||
-      origin === "http://localhost:5173"
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
+//     if (
+//       origin.includes("vercel.app") ||
+//       origin === "http://localhost:5173"
+//     ) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
 
-// 🔥 USE CORS (ONLY ONCE)
-app.use(cors(corsOptions));
+// // 🔥 USE CORS (ONLY ONCE)
+// app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 
-// app.use(cors({
-//   origin: [
-//     'https://mern-estate-frontend-eight.vercel.app', 
-//     'http://localhost:5173'
-//   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: [
+    'https://mern-estate-frontend-blush.vercel.app', 
+    'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // mongoose.connect("mongodb+srv://meerabshahid270_db_user:m3hUumpcwVGE7OVk@cluster0.rpk2uw0.mongodb.net/?appName=Cluster0")
 // mongoose.connect("mongodb://127.0.0.1:27017/mern-estate") -------->local compass
@@ -73,6 +73,6 @@ app.use((err, req, res, next)=>{
 });
 // 
   // Server
-  // const PORT = process.env.PORT || 3000;
-  // app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 module.exports=app;
