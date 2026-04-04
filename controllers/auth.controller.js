aa9f91039adf00ac7a532ecfd21e1d69b351e91b
MeerabShahid18 <meerabshahid866@gmail.com>
2026-04-04 15:08:52 +0500

    updating cookie response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,7 +25,11 @@ const signin=async(req, res, next)=>{
         const token=jwt.sign({id: validUser._id},process.env.JWT_SECRET);
         const {password:pass, ...rest}=validUser._doc;
         res
-        .cookie('access_token', token,{httpOnly: true }) 
+        .cookie('access_token', token,{
+            httpOnly: true,
+            sameSite: 'none',   // 🔥 MUST for cross-origin (localhost → vercel)
+            secure: true,       // 🔥 MUST for HTTPS (Vercel)
+        }) 
         .status(200)
         .json(rest);
     }catch(e){
